5e104a427d51ad3814172c9012d015d610d46c96
andrew <andrewtclarkson@gmail.com>
2022-06-08 18:49:50 -0400

    Install Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import "./styles.css";
-import { JobBoardListings } from "./components/JobBoardListings";
+import './styles.css';
+import { JobBoardListings } from './components/JobBoardListings';
+import TopNav from './components/TopNav';
 
 export default function App() {
   return (
-    <div className="layout">
-      <div className="header">Header</div>
-      <main className="main">
-        <h1>FirstJobs.dev</h1>
+    <div className='layout'>
+      <TopNav />
+      {/* <div className='header'>Header</div> */}
+
+      <main className='main'>
+        {/* <h1>FirstJobs.dev</h1> */}
         <h2>
           An aggregation of companies looking to hire first workers with no
           experience required.
         </h2>
         <JobBoardListings />
       </main>
-      <footer className="footer">Footer</footer>
+      <footer className='footer'>Footer</footer>
     </div>
   );
 }
