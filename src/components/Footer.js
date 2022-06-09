3567d27eda7a8ffcd8f0fda41b54f69427b4b540
andrew <andrewtclarkson@gmail.com>
2022-06-09 09:43:26 -0400

    make footer linkers open new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,11 +7,19 @@ const Footer = () => {
       <Box mt={2} textAlign='center'>
         <Typography>
           Built by
-          <a href='https://www.linkedin.com/in/chris-naismith/'>
+          <a
+            href='https://www.linkedin.com/in/chris-naismith/'
+            target='_blank'
+            rel='noreferrer'
+          >
             🌲Chris Naismith
           </a>{' '}
           &{' '}
-          <a href='https://www.linkedin.com/in/andrewtclarkson/'>
+          <a
+            href='https://www.linkedin.com/in/andrewtclarkson/'
+            target='_blank'
+            rel='noreferrer'
+          >
             Andrew Clarkson
           </a>
         </Typography>
